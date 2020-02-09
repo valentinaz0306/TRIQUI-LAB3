9ea92ad745199241a6b2a00a6b3d8bad66bfdf7c
Valentina Zapata <valentinazapata@MacBook-Pro-de-Valentina-2.local>
2020-02-09 16:38:05 -0500

    logica full

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 let matriz = new Array(3);
 let turno=0;
+let ganador = 0;
 
 function setup(){
 createCanvas(600,600);
@@ -27,12 +28,19 @@ for (i=0;i<3;i++){
     for(j=0;j<3;j++){
         matriz[i][j].pintarse();
     }
-}    
+} 
+ganador = winner(); 
+if(ganador!=0){
+    textSize(30);
+    fill(0,255,0);
+    text("GANASTEEE JUGADOR" + ganador,50,300);
+}
 }
 function mousePressed(){
     // tipo 0 vacio
    //tipo 1 circulo
    //tipo 2 cuadrado
+if (ganador==0){
 
 if(mouseX>0&&mouseX<200 && mouseY>0 && mouseY<200 && matriz[0][0].getTipo()==0){
     matriz[0][0].setTipo(turno+1);
@@ -70,28 +78,37 @@ if(mouseX>400 && mouseX<600 && mouseY>400&& mouseY<600 && matriz[2][2].getTipo()
     matriz[2][2].setTipo(turno+1);
     turno=(turno+1)%2;
 }
+}// cierre if 
 }
 function winner(){
+    let win = 0;
     // ganar filas
     if(matriz[0][0].getTipo()==matriz[0][1].getTipo() && matriz[0][1].getTipo()==matriz[0][2].getTipo()){
-        //win first row 
+        win = matriz[0][0].getTipo();
     }
     if(matriz[1][0].getTipo()==matriz[1][1].getTipo() && matriz[1][1].getTipo()==matriz[1][2].getTipo()){
+        win = matriz[1][0].getTipo();
     }
     if(matriz[2][0].getTipo()==matriz[2][1].getTipo() && matriz[2][1].getTipo()==matriz[2][2].getTipo()){
+        win = matriz[2][0].getTipo();
     }
      // ganar col
      if(matriz[0][0].getTipo()==matriz[1][0].getTipo() && matriz[1][0].getTipo()==matriz[2][0].getTipo()){
+        win = matriz[0][0].getTipo();
     }
     if(matriz[0][1].getTipo()==matriz[1][1].getTipo() && matriz[1][1].getTipo()==matriz[2][1].getTipo()){
+        win = matriz[0][1].getTipo();
     }
     if(matriz[0][2].getTipo()==matriz[1][2].getTipo() && matriz[1][2].getTipo()==matriz[2][2].getTipo()){
+        win = matriz[0][2].getTipo();
     }
     //ganar diagonal 
     if(matriz[0][2].getTipo()==matriz[1][1].getTipo() && matriz[1][1].getTipo()==matriz[2][0].getTipo()){
+        win = matriz[0][2].getTipo();
     }
     if(matriz[0][0].getTipo()==matriz[1][1].getTipo() && matriz[1][1].getTipo()==matriz[2][2].getTipo()){
+        win = matriz[0][0].getTipo();
     }
-
+    return win;
 }
 
